af006e61aa8e373323bb79c77a4118b3f2d55c0e
Manoj Gautam <manojit.gautam@gmail.com>
2019-11-29 05:10:12 +0545

    c++ crash course files

---
+++ 00_arith.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+
+int main () {
+
+	float cubeSide = 5.4;
+	float sphereRadius = 2.33;
+	float coneRadius = 7.65; 
+	float coneHeight = 14; 
+
+	// Volume Calculation
+	float volCube, volSphere, volCone = 0;
+	
+
+	volCube = std::pow(cubeSide, 3);
+	volSphere = (4.0/3.0) * M_PI * std::pow(sphereRadius, 3); // M_PI is in MATH
+	volCone = M_PI * std::pow(coneRadius, 2) * (coneHeight/3);
+
+	// Output
+	std::cout<<"\nVolume of the cube:"<<volCube<<"\n";
+	std::cout <<"\nVolume of Sphere: "<<volSphere<<"\n";
+  std::cout <<"\nVolume of Cone: "<<volCone<<"\n";
+	return 0;
+
+	}
---
+++ 00_hello.cpp
@@ -0,0 +1,10 @@
+// helloworld.cpp 
+
+#include <iostream> // C++ standard headers don't use ".h" 
+
+int main(int argc, char** argv) {
+	
+	std::cout<< "Hello world!"<<std::endl;
+
+	return 0; 
+}
