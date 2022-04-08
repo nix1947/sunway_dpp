c134ea46f545ca7eb88bb2cf400faf9b60008085
Manoj Gautam <nix1947@users.noreply.github.com>
2022-04-08 07:39:50 +0545

    Update forloop.c

--- openmp/forloop.c
+++ openmp/forloop.c
@@ -2,27 +2,21 @@
 #include "omp.h" 
 
 void main () {
-// Decleare 3 array
-long long i, k, N=1000000000000; 
+
+long long i, k, N=100000; 
 long long A = 0;
 long B = 100;
-long C 20;
+long C = 20;
 
-
-// Populate the Array B and C with some random numbers. 
-// You can use forloops to populate the array. 
-
-
-// Run the forloop and parallelize it. 
 
 #pragma omp parallel 
 {
 for(i=0; i<N; i++) {
-			B = i * 2; // For initilizing the value in Array B
-      C = i + 2; // For initilizing the value in Array C 
+	B = i * 2; // For initilizing the value in Array B
+	C = i + 2; // For initilizing the value in Array C 
 
-			A += B + k * C;
-		 printf("%l", A);
+	A += B + k * C;
+	printf("%l", A);
   }
 
 }
